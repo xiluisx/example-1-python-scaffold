0a0a9e39f96342c7d98a626bcb6938c82331a2dd
Luis <67877206+xiluisx@users.noreply.github.com>
2026-08-26 10:31:12 -0600

    fix: fixed test

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
+import random
+import hashlib
+
+
 def add(x, y):
     """This is an add function"""
     return x + y
 
 
+def random_hash():
+    """Generate a random SHA-256 has."""
+    random_value = str(random.random())
+    return hashlib.sha256(random_value.encode()).hexdigest()
+
+
 if __name__ == "__main__":
-    print(add(1, 1))
+    print(random_hash())
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,13 @@
-from hello import add
+from hello import add, random_hash
+import hashlib
 
 
 def test_add():
     assert 2 == add(1, 1)
+
+
+def test_random_has():
+    result = random_hash()
+
+    assert isinstance(result, str)
+    assert len(result) == 64
